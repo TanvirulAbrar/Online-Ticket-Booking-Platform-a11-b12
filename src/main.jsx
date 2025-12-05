423a4640941b9ab6a16c1e67005b29c18f4396db
TanvirulAbrar <abrartanvirul@gmail.com>
2025-12-05 21:23:48 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
